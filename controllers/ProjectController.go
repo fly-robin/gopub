150c39f84e7910c9f7b78729ff938e194fcdb8f8
danxian <danxian@staff.sina.com.cn>
2018-05-28 09:58:38 +0800

    完善gitinfo结构体数据

--- controllers/ProjectController.go
+++ controllers/ProjectController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/fly-robin/gopub/models"
 	"github.com/fly-robin/gopub/utils"
+	"github.com/astaxie/beego"
 )
 
 type ProjectController struct {
@@ -15,7 +16,11 @@ func (c *ProjectController) ProjectPanel() {
 	c.Prepare()
 	projectDetail, err := models.GetProjectById(c.currentProject)
 	utils.CheckError(err)
-
+	projectPath := projectDetail.RepositoryPath
+	//获取当前所在git相关信息
+	gitInfo := utils.GetGitInfo(projectPath)
+	c.Data["git_branch"] = gitInfo.Branch
+	c.Data["git_tag"] = gitInfo.Tag
 	c.Data["project_detail"] = projectDetail
 	c.TplName = "project/detail.html"
 	c.Render()
--- utils/Git.go
+++ utils/Git.go
@@ -2,21 +2,52 @@ package utils
 
 import (
 	"regexp"
-	"fmt"
+	"strings"
 )
 
 //
 type GitInfo struct {
-	branch string //当前所在分支
+	Branch GitBranch //分支信息
+	Tag GitTag
+}
 
+type GitBranch struct{
+	Origin string
+	Local string
+}
+
+type GitTag struct{
+	Tags []string
+}
+
+
+func GetGitInfo(projectPath string) GitInfo {
+	localBranch, originBranch := GetCurrentBranch(projectPath)
+	gitBranch := GitBranch{
+		originBranch,
+		localBranch,
+	}
+
+	gitTags := GetGitTags(projectPath)
+	gitTag := GitTag{
+		gitTags,
+	}
+
+	var gitInfo GitInfo
+	gitInfo.Branch = gitBranch
+	gitInfo.Tag = gitTag
+
+	return gitInfo
 }
 
 //获取当前的分支名
 func GetCurrentBranch(projectPath string)  (string, string) {
-	cmommand :=  "cd " + projectPath + " & git status"
-	stringResult, err := ExecShell(cmommand)
+	command :=  "cd " + projectPath + " && git status"
+	stringResult, err := ExecShell(command)
 	CheckError(err)
+	//本地分支
 	localBranch := findLocalBranchFromGitStatus(stringResult)
+	//远程分支
 	originBranch := findOriginBranchFromGitStatus(stringResult)
 
 	return localBranch, originBranch
@@ -24,7 +55,6 @@ func GetCurrentBranch(projectPath string)  (string, string) {
 
 //从git status结果中解析出本地分支名
 func findLocalBranchFromGitStatus(gitStatus string) string {
-	fmt.Println(gitStatus)
 	reg, err := regexp.Compile("branch (.*)\n");
 	subMatch := reg.FindStringSubmatch(gitStatus)
 	CheckError(err)
@@ -38,7 +68,6 @@ func findLocalBranchFromGitStatus(gitStatus string) string {
 
 //解析出对应的远程分支
 func findOriginBranchFromGitStatus(gitStatus string) string {
-	fmt.Println(gitStatus)
 	reg, err := regexp.Compile("\\'(.*)\\'");
 	subMatch := reg.FindStringSubmatch(gitStatus)
 	CheckError(err)
@@ -52,12 +81,13 @@ func findOriginBranchFromGitStatus(gitStatus string) string {
 
 
 //获取当前工程的所有tag
-func GetGitTags(projectPath string) string {
+func GetGitTags(projectPath string) []string {
 	commandString := "cd " + projectPath + " & git tag -l"
-	result, err := ExecShell(commandString)
+	gitTagsString, err := ExecShell(commandString)
 	CheckError(err)
+	gitTagsArray := strings.Split(gitTagsString, "\n")
 
-	return result
+	return gitTagsArray
 }
 
 //更新git本本地仓库
